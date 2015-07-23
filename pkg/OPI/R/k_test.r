ac11703b555ebf1fb6a7322b9511668e47508509
Andrew Turpin <aturpin@unimelb.edu.au>
2015-07-23 15:30:48 +1000

    commented out k_test so R CMD check will work.

--- pkg/OPI/R/k_test.r
+++ pkg/OPI/R/k_test.r
@@ -1,29 +1,29 @@
 
 # install.package("OPI_2.2.tar.gz", NULL, type="source")
 
-require(OPI)
-
-chooseOpi("KowaAP7000")
-
-res <- opiInitialise()
-if (!is.null(res))
-    stop('opiInitialise failed')
-
-makeStim <- function(db) { 
-     s <- list(x=9, y=9, level=dbTocd(db, 10000/pi), size=26/60,
-                color=.KowaAP7000Env$COLOR_WHITE,
-                duration=200, responseWindow=1500)
-     class(s) <- "opiStaticStimulus"
-     
-     return(s)
-}
-     
-#res <- opiPresent(stim=makeStim(10), nextStim=NULL)
-#print(res)
-
-res <- opiClose()
-if (!is.null(res)) {
-    print("opiClose() failed")
-} else {
-    print("opiClose() succeeded")
-}
+### require(OPI)
+### 
+### chooseOpi("KowaAP7000")
+### 
+### res <- opiInitialise()
+### if (!is.null(res))
+###     stop('opiInitialise failed')
+### 
+### makeStim <- function(db) { 
+###      s <- list(x=9, y=9, level=dbTocd(db, 10000/pi), size=26/60,
+###                 color=.KowaAP7000Env$COLOR_WHITE,
+###                 duration=200, responseWindow=1500)
+###      class(s) <- "opiStaticStimulus"
+###      
+###      return(s)
+### }
+###      
+### #res <- opiPresent(stim=makeStim(10), nextStim=NULL)
+### #print(res)
+### 
+### res <- opiClose()
+### if (!is.null(res)) {
+###     print("opiClose() failed")
+### } else {
+###     print("opiClose() succeeded")
+### }
